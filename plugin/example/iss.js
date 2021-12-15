9f3523a71c72d73b2559bae55626c7f2f3d1af77
lavalse <lavalselike@gmail.com>
2021-12-15 11:55:02 +0900

    init

---
+++ plugin/example/iss.js
@@ -0,0 +1,78 @@
+const getText = () => reearth.widget.property && reearth.widget.property.default ? reearth.widget.property.default.text || "" : "";
+const html = `
+<div id="wrapper">
+<h1>Current ISS location</h1>
+<p>Latitude: <span id="lat">-</span></p>
+<p>Longitude: <span id="lon">-</span></p>
+<p>Altitude: <span id="alt">-</span>km</p>
+<p><button id="update">Update</button> <button id="jump">Jump!</button></p>
+</div>
+<style>
+  body {
+    margin: 0;
+    width: 300px;
+  }
+  body.extended {
+    width: 100%;
+    height: 100%;
+  }
+  #wrapper {
+    border: 2px solid blue;
+    border-radius: 5px;
+    background-color: rgba(111, 111, 111, 0.5);
+    box-sizing: border-box;
+  }
+  body.extended #wrapper {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+<script>
+  let lat, lng, alt;
+  const update = () => {
+    fetch("https://api.wheretheiss.at/v1/satellites/25544").then(r => r.json()).then(data => {
+      lat = data.latitude;
+      lng = data.longitude;
+      alt = data.altitude;
+      document.getElementById("lat").textContent = data.latitude;
+      document.getElementById("lon").textContent = data.longitude;
+      document.getElementById("alt").textContent = data.altitude;
+    });
+  };
+  document.getElementById("update").addEventListener("click", update);
+  document.getElementById("jump").addEventListener("click", () => {
+    if (lat === undefined) return;
+    parent.postMessage({ lat, lng, alt }, "*");
+  });
+
+  const extended = ${JSON.stringify(!!reearth.widget.extended)};
+  if (extended) {
+    document.body.classList.add("extended");
+  }
+  addEventListener("message", e => {
+    if (e.source !== parent) return;
+    if (e.extended) {
+      document.body.classList.add("extended");
+    } else {
+      document.body.classList.remove("extended");
+    }
+  });
+
+  update();
+</script>
+`;
+
+reearth.ui.show(html);
+reearth.on("update", () => {
+  reearth.ui.postMessage({ extended: !!reearth.widget.extended });
+});
+reearth.on("message", msg => {
+  const flyTo = reearth.visualizer.flyTo ?? reearth.visualizer.camera.flyTo;
+  flyTo({
+    lat: msg.lat,
+    lng: msg.lng,
+    alt: msg.alt,
+  }, {
+    duration: 2
+  });
+});
---
+++ plugin/example/red_text.js
@@ -0,0 +1,31 @@
+const getText = () => reearth.widget.property && reearth.widget.property.default ? reearth.widget.property.default.text || "" : "";
+const html = `
+<div id="wrapper">
+<h1 id="text"></h1>
+</div>
+<style>
+  html, body {
+  }
+  #wrapper {
+    background-color: rgba(111, 111, 111, 0.5);
+    margin: -10px;
+    border: solid red;
+    width: 50px;
+  }
+</style>
+<script>
+  const cb = text => {
+    document.getElementById("text").textContent = text;
+  };
+  addEventListener("message", e => {
+    if (e.source !== parent) return;
+    cb(e.data);
+  });
+  cb(${JSON.stringify(getText())});
+</script>
+`;
+
+reearth.ui.show(html);
+reearth.on("update", () => {
+  reearth.ui.postMessage(getText());
+});
